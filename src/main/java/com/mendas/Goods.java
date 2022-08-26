dc9069fb730358f471915cbf7bce1a783da2f192
joker <jianhao.xie@tongdun.net>
2022-08-27 01:15:59 +0800

    陈年干酪测试

--- src/main/java/com/mendas/Goods.java
+++ src/main/java/com/mendas/Goods.java
@@ -5,11 +5,12 @@ package com.mendas;
  * @date 2022年08月26日 11:42 下午
  */
 public class Goods {
-
+    private String name;
     private double quality;
     private int sellIn;
 
     public Goods(String name, double quality, int sellIn) {
+        this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
 
@@ -17,14 +18,33 @@ public class Goods {
 
     public void updateByDay() {
 
+        if ("Aged Brie".equals(name)) {
+            increaseQuality();
+            decreaseSellIn();
+            if (sellIn < 0) {
+                increaseQuality();
+
+            }
+            return;
+        }
         decreaseQuality();
-
-        sellIn = sellIn - 1;
-
+        decreaseSellIn();
         if (sellIn < 0) {
             decreaseQuality();
         }
 
+    }
+
+    private void increaseQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
+
+
+
+    private void decreaseSellIn() {
+        sellIn = sellIn - 1;
     }
 
 
---
+++ src/test/java/cn/gilded_rose/AgedBrieTest.java
@@ -0,0 +1,83 @@
+package cn.gilded_rose;
+
+import com.mendas.Goods;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author mendas
+ * @date 2022年08月27日 12:46 上午
+ */
+public class AgedBrieTest {
+    /**
+     * given 没有过保,价值小于50 when按天更新 then保质期减1 价值加1
+     */
+    @Test
+    void test() {
+        double oldQuality = 45;
+        int oldSellIn = 1;
+        Goods goods = new Goods("Aged Brie", oldQuality, oldSellIn);
+        goods.updateByDay();
+
+        double newQuality = goods.getQuality();
+        int newSellIn = goods.getSellIn();
+
+        assertEquals(newQuality, oldQuality + 1);
+        assertEquals(newSellIn, oldSellIn - 1);
+    }
+
+
+    /**
+     * given 没有过保,价值等于50 when按天更新 then保质期减1 价值任为50
+     */
+    @Test
+    void test2() {
+        double oldQuality = 50;
+        int oldSellIn = 1;
+        Goods goods = new Goods("Aged Brie", oldQuality, oldSellIn);
+        goods.updateByDay();
+
+        double newQuality = goods.getQuality();
+        int newSellIn = goods.getSellIn();
+
+        assertEquals(newQuality, 50);
+        assertEquals(newSellIn, oldSellIn - 1);
+    }
+
+
+    /**
+     * given 过保,价值小于49 when按天更新 then保质期减1 价值加2
+     */
+    @Test
+    void test3() {
+        double oldQuality = 45;
+        int oldSellIn = 0;
+        Goods goods = new Goods("Aged Brie", oldQuality, oldSellIn);
+        goods.updateByDay();
+
+        double newQuality = goods.getQuality();
+        int newSellIn = goods.getSellIn();
+
+        assertEquals(newQuality, oldQuality+2);
+        assertEquals(newSellIn, oldSellIn - 1);
+    }
+
+    /**
+     *given 过保,价值等于49 when按天更新 then保质期减1 价值加50
+     */
+    @Test
+    void test4() {
+        double oldQuality = 49;
+        int oldSellIn = 0;
+        Goods goods = new Goods("Aged Brie", oldQuality, oldSellIn);
+        goods.updateByDay();
+
+        double newQuality = goods.getQuality();
+        int newSellIn = goods.getSellIn();
+
+        assertEquals(newQuality, 50);
+        assertEquals(newSellIn, oldSellIn - 1);
+    }
+
+}
